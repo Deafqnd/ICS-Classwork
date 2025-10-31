60f764282356c09373ce1d939e8df78ffa2360ab
Deafqnd <ayden.m.chan@gmail.com>
2025-10-31 09:17:39 -0400

    Create again_guessing.py

---
+++ again_guessing.py
@@ -0,0 +1,20 @@
+import random
+
+number = random.randrange(1, 11)
+tries = 0
+
+print('I have chosen a number between 1 and 10. Try to guess it!')
+guess = int(input('Your guess: '))
+
+guess == number
+
+while False:
+    print('That is incorrect. Try again!')
+    guess = int(input('Your guess: '))
+    tries += 1
+
+    if guess == number:
+        break
+
+print('That\'s right! You\'re a good guesser!')
+print(f'You only took {tries} tries!')
