74d756310d03b0a2bec789cf5881db0d49616d79
Deafqnd <ayden.m.chan@gmail.com>
2025-11-13 08:45:37 -0500

    Create thresholding.py

---
+++ thresholding.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np 
+
+img = cv2.imread('bookpage.jpg')
+grayscaled = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+th = cv2.adaptiveThreshold(grayscaled, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+retval, threshold = cv2.threshold(grayscaled, 10, 255, cv2.THRESH_BINARY)
+cv2.imshow('original',img)
+cv2.imshow('threshold',threshold)
+cv2.waitKey(0)
+cv2.destroyAllWindows
